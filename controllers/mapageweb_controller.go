94868d237e19d3e1927600c7dee44e83b362caf5
Mohamed98hlt <DRGX7717@orange.com>
2023-04-13 10:22:46 +0200

    1st

--- controllers/mapageweb_controller.go
+++ controllers/mapageweb_controller.go
@@ -72,7 +72,7 @@ func (r *MaPageWebReconciler) Reconcile(ctx context.Context, req ctrl.Request, M
 	configMap := &corev1.ConfigMap{
 		ObjectMeta: metav1.ObjectMeta{
 			Name: MaPageWeb.Name + "-config",
-		} ,,  Data: data ,
+		} ,
 	 }
 
 	// Set MyResource instance as the owner and controller of the ConfigMap
